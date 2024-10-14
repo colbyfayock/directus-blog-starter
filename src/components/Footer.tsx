7f51ff03c33de26a3054914be3c0ff4db0f2f940
Colby Fayock <fayock@gmail.com>
2024-10-14 13:47:14 -0400

    footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="mt-12 mb-8">
       <Container className="flex justify-between gap-4">
         <p className="text-sm">Space Jelly &copy; {new Date().getFullYear()}</p>
-        <p className="text-sm">Created by Colby Fayock with Directus</p>
+        <p className="text-sm">Created by Colby Fayock with <a href="https://directus.io/?ref=spacejelly">Directus</a></p>
       </Container>
     </footer>
   );
